1f6721defb51eab6ee1ffa8432421de266dc7b01
bobrinik <bobrinik@pm.me>
2023-11-07 22:05:29 -0500

    Add financail statement fetching for FMP client

--- finclaw/vendor/fmp/fmp_client.py
+++ finclaw/vendor/fmp/fmp_client.py
@@ -17,7 +17,14 @@ async def _call_api(session, url: str, payload: str, querystring: dict, error_co
     if settings.FMP_API_KEY == "":
         raise ValueError("You need to specify FMP_API_KEY")
     querystring["apikey"] = settings.FMP_API_KEY
+
     async with session.get(url, data=payload, params=querystring) as resp:
+        if error_count >= 4:
+            raise ValueError(f"Too many errors: {resp.status}")
+        if resp.status != 200:
+            logger.info("Rate limit reached going to sleep")
+            await asyncio.sleep(60)
+            return await _call_api(session, url, payload, querystring, error_count + 1)
         try:
             json_response = await resp.json()
         except aiohttp.ContentTypeError as e:
@@ -42,3 +49,23 @@ async def get_stock_candle(session: aiohttp.ClientSession, symbol: str, resoluti
         )
     else:
         raise NotImplementedError("Only 1 minute resolution is supported")
+
+
+async def get_financials_for(session: aiohttp.ClientSession, symbol: str, statement: str, freq: str):
+    """
+    https://site.financialmodelingprep.com/developer/docs#financial-statements
+    """
+    endpoint_map = {
+        "bs": "balance-sheet-statement",
+        "ic": "income-statement",
+        "cf": "cash-flow-statement",
+    }
+    periods = ["annual", "quarter"]
+    if statement not in endpoint_map:
+        raise ValueError(f"statement:{statement} should be one of bs, ic, cf")
+    if freq not in periods:
+        raise ValueError(f"freq:{freq} should be one of {periods}")
+
+    return await _call_api(
+            session, f"{API_BASE}{endpoint_map[statement]}/{symbol}", payload="", querystring={"period": freq}
+        )
