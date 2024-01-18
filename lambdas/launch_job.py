8faeed682086b6ee9b8d39b00fb1274ebb7d4209
Maksim Bober <bobmaxal@hotmail.com>
2024-01-17 22:54:15 -0500

    Add lambda code for triggering batch job

---
+++ lambdas/launch_job.py
@@ -0,0 +1,38 @@
+import boto3
+from datetime import datetime, timedelta
+import logging
+import json
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+client = boto3.client('batch')
+JOB_QUE_NAME = "main"
+
+start = datetime.now().utcnow() - timedelta(days=1)
+end = datetime.now().utcnow()
+start = start.strftime('%Y-%m-%dT%H:%M:%S')
+end = end.strftime('%Y-%m-%dT%H:%M:%S')
+
+
+def start_job(vendor):
+    response = client.submit_job(
+        jobName=f'fetch-{vendor}-job-{datetime.now().strftime("%Y-%m-%d-%H_%M")}',
+        jobQueue=JOB_QUE_NAME,
+        jobDefinition='finclaw-fetch',
+        containerOverrides={
+            'command': [
+                'python', 'tools/run_daily_eod.py', '--start', start, '--end', end, '--vendor', vendor
+            ],
+        }
+    )
+    logger.info("Submitted job: {}".format(response['jobId']))
+    logger.info(f"Response: {response}")
+
+
+def lambda_handler(event, context):
+    resp = start_job(event["vendor"])
+    return {
+        'statusCode': 200,
+        'body': json.dumps(resp)
+    }
