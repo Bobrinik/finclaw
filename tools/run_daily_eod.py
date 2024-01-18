ea06ddf111a4f25b362946640f469160d67b5522
Maksim Bober <bobmaxal@hotmail.com>
2024-01-17 22:53:30 -0500

    Update starting script

--- tools/run_daily_eod.py
+++ tools/run_daily_eod.py
@@ -1,10 +1,23 @@
 from datetime import datetime, timedelta
 import subprocess
+import argparse
 
+
+"""
+Example:
+    finclaw grab --start 2024-01-17 --end 2024-01-17 --frequency 1  --market TO -ic p -v fmp
+"""
 if __name__ == '__main__':
-    start = datetime.now().utcnow() - timedelta(days=1)
-    end = datetime.now().utcnow()
+    parser = argparse.ArgumentParser(description="Process some named arguments.")
+
+    # Define the named arguments
+    parser.add_argument('--start', type=str, help='Your name')
+    parser.add_argument('--end', type=int, help='Your age')
+    parser.add_argument('--vendor', type=str, help='Country of residence')
+
+    # Parse the arguments
+    args = parser.parse_args()
 
     subprocess.run(
-        f"finclaw grab --start {start.strftime('%Y-%m-%dT%H:%M:%S')} --end {end.strftime('%Y-%m-%dT%H:%M:%S')} --frequency 1  --market TO -ic p -v fmp",
+        f"finclaw grab --start {args.start} --end {args.end} --frequency 1  --market TO -ic p -v {args.vendor}",
         shell=True)
