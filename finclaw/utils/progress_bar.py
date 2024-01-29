4b5cea669d67e8ccc867ca693c0392dc4c49077e
Maksim Bober <bobmaxal@hotmail.com>
2024-01-28 22:22:47 -0500

    Update progress bar

--- finclaw/utils/progress_bar.py
+++ finclaw/utils/progress_bar.py
@@ -2,20 +2,29 @@ from tqdm import tqdm
 from finclaw.config import settings, logger
 
 
-def simple_progress_bar(iterable):
+def simple_progress_bar(iterable, desc: str = None):
     counter = 0
     for i in iterable:
         if counter % 55 == 0:
-            logger.info(counter)
+            if desc:
+                logger.info(counter)
+            else:
+                logger.info(f"{desc}:{counter}")
         counter += 1
         yield i
 
 
-def get_progress_bar():
+def get_progress_bar(desc: str = None):
     if settings.USE_TQDM:
-        return lambda x: tqdm(x)
+        if desc:
+            return lambda x: tqdm(x, desc=desc)
+        else:
+            return lambda x: tqdm(x)
     else:
-        return simple_progress_bar
+        if desc:
+            return lambda itr: simple_progress_bar(itr, desc=desc)
+        else:
+            return simple_progress_bar
 
 
 progress_bar = get_progress_bar()
