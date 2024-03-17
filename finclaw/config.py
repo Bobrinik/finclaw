df8f2cdae3ca792674d6284c830a2b7939a3f904
Maksim Bober <bobmaxal@hotmail.com>
2024-03-17 16:59:01 -0400

    Improve dev ergonomics

--- finclaw/config.py
+++ finclaw/config.py
@@ -4,9 +4,13 @@ import os
 from pydantic import BaseSettings
 from dotenv import load_dotenv
 
+logger = logging.getLogger()
+logging.basicConfig(level=logging.DEBUG)
+
 
 def is_running_in_jupyter():
     try:
+        # get_ipython is defined when this is called from inside Jupyter
         shell = get_ipython().__class__.__name__
         if shell == "ZMQInteractiveShell":
             return True  # Jupyter Notebook
@@ -34,13 +38,15 @@ class Settings(BaseSettings):
     KRAKEN_API_SECRET: str
     USE_TQDM: bool = False
 
+    # It can be set when using AWS
+    S3_BUCKET_NAME: str = ""
+
 
 if is_running_in_jupyter():
+    logger.info("Running inside Jupyter")
+    logger.info("Note that .env file should be in the same directory as this notebook")
     load_dotenv()
     settings = Settings()
 else:
     load_dotenv()
     settings = Settings()
-
-logger = logging.getLogger()
-logging.basicConfig(level=logging.DEBUG)
